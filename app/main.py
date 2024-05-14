fb332ac54c2b04ce26fe781a880cf75610860e7d
Shaun Chen <motestw64631@gmail.com>
2024-05-15 00:34:28 +0800

    rate limit with redis

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 import uvicorn
 from fastapi import FastAPI, Request, status
-from app.user.schemas import UserCreate, UserVerify
+from app.user.schemas import UserCreate, UserVerify, ResponseModel, ErrorResponse
 from app.user import crud
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
@@ -11,9 +11,11 @@ from fastapi.exceptions import RequestValidationError
 from fastapi import FastAPI, Depends
 from datetime import datetime, timedelta
 from functools import wraps
+import traceback
+import redis
 
 app = FastAPI()
-
+redis_client = redis.Redis(host='redis', port=6379, decode_responses=True)
 
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(request: Request, exc: RequestValidationError):
@@ -30,6 +32,23 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
                }),
     )
 
+@app.exception_handler(Exception)
+async def generic_exception_handler(request: Request, exc: Exception):
+    traceback.print_exc()
+    return JSONResponse(
+        status_code=500,
+        content=jsonable_encoder({
+               "success":False,
+               "reason": "Oops! Something Went Wrong"
+               })
+    )
+
+@app.exception_handler(HTTPException)
+async def http_exception_handler(request: Request, exc: HTTPException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content=jsonable_encoder(ErrorResponse(success=False, reason=exc.detail))
+    )
 
 
 password_failures = {}
@@ -37,7 +56,6 @@ password_failures = {}
 def attempt_limiter(endpoint):
     @wraps(endpoint)
     async def wrapper(request: Request, *args, **kwargs):
-        global password_failures
         LOCKOUT_DURATION = timedelta(minutes=1)
         # Get the client's IP address
         client_ip = request.client.host
@@ -49,32 +67,34 @@ def attempt_limiter(endpoint):
         current_datetime = datetime.now()
 
         # Attempt to retrieve the failure attempts for the user
-        failure_data = password_failures.get(user_key, None)
+        failure_data = redis_client.hgetall(user_key)
         if not failure_data:
-            password_failures[user_key] = {"attempts": 1, "lockout_time": None}
-        failure_data = password_failures[user_key]
-
-        if failure_data["attempts"] >= 5:
+            # Initialize failure data if not present
+            redis_client.hmset(user_key, {"attempts": 1, "lockout_time": ""})
+        failure_data = redis_client.hgetall(user_key)
+        if int(failure_data["attempts"]) >= 5:
             # If there are 5 or more failure attempts
-            if failure_data["lockout_time"] is None:
-                # Set lockout_time when attempts firt time > 5
-                password_failures[user_key] = {"attempts": 5, "lockout_time": current_datetime + LOCKOUT_DURATION}
+            if not failure_data["lockout_time"]:
+                # Set lockout_time when attempts first time > 5
+                redis_client.hmset(user_key, {"attempts": 5, "lockout_time": (current_datetime + LOCKOUT_DURATION).timestamp()})
+            elif current_datetime >= datetime.fromtimestamp(float(failure_data["lockout_time"])):
                 # Remove limit when user wait > 1 min
-            elif current_datetime >= failure_data["lockout_time"]:
-                password_failures[user_key] = {"attempts": 1, "lockout_time": None}
+                redis_client.hmset(user_key, {"attempts": 1, "lockout_time": ""})
             else:
-                # Raise Exception
-                raise HTTPException(status_code=429, detail=f"Too many password verification failures. Please wait until {failure_data['lockout_time']} before trying again.")
+                raise HTTPException(status_code=429, detail=f"Too many password verification failures. Please wait until {datetime.fromtimestamp(float(failure_data['lockout_time']))} before trying again.")
 
         # Increment attempts
-        failure_data["attempts"] += 1
+        redis_client.hincrby(user_key, "attempts", 1)
 
         return endpoint(request, *args, **kwargs)
 
     return wrapper
 
 
-@app.post("/users")
+@app.post("/users", response_model=ResponseModel, responses={
+    400: {"model": ErrorResponse},
+    422: {"model": ErrorResponse}
+})
 def create_user(user:UserCreate, db: Session = Depends(get_db)):
    db_user = crud.get_user_by_username(db, username=user.username)
    if db_user:
@@ -83,7 +103,11 @@ def create_user(user:UserCreate, db: Session = Depends(get_db)):
    return JSONResponse(status_code=200, content={"success": True})
    
    
-@app.post("/verify")
+@app.post("/verify", response_model=ResponseModel, responses={
+    401: {"model": ErrorResponse},
+    422: {"model": ErrorResponse},
+    429: {"model": ErrorResponse}
+})
 @attempt_limiter
 def verify_user(request: Request, user:UserVerify, db: Session = Depends(get_db)):
    verify = crud.verify_user(db=db, user=user)
